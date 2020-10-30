216fde002955f53cc8fc0a94e94a4a1ae226026e
Yvan Sabay <sabayyvan2018@gmail.com>
2020-10-30 16:40:06 +0800

    Added FE4 Activity

---
+++ FE4/assets/js/app.js
@@ -0,0 +1,63 @@
+
+var ctx = document.getElementById('barChart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['JAN', 'FEB', 'MARCH', 'APRIL', 'MAY', 'JUNE'],
+    datasets: [{
+      label: ['Borrowed Books', ' Returned Books'],
+      data: [7, 10, 9, 12, 13, 6],
+      backgroundColor: [
+        'rgba(117, 121, 231, .8)',
+        'rgba(154, 179, 245, .8)',
+        'rgba(117, 121, 231, .8)',
+        'rgba(154, 179, 245, .8)',
+        'rgba(117, 121, 231, .8)',
+        'rgba(154, 179, 245, .8)',
+      ],
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
+
+var pie = document.getElementById('pieChart').getContext('2d');
+var pieChart = new Chart(pie, {
+  type: 'doughnut',
+  data: {
+    datasets: [{
+        data: [10, 20, 30, 20],
+        backgroundColor: [
+          'rgb(52, 152, 219, .8)',
+          'rgba(154, 179, 245, .8)',
+          'rgba(27, 206, 230, .8)',
+          'rgba(27, 64, 230, .8)',
+        ],
+    }],
+
+    labels: [
+        'Novel',
+        'Fiction',
+        'Biography',
+        'Horror',
+    ]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
+
+
