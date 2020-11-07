cb48e9983345f77b23d398c5cfb2ee15ac03ab60
Yvan Sabay <sabayyvan2018@gmail.com>
2020-11-07 22:53:07 +0800

    Final FE4

--- FE4/assets/js/app.js
+++ FE4/assets/js/app.js
@@ -1,21 +1,21 @@
 
 var ctx = document.getElementById('barChart').getContext('2d');
-var myChart = new Chart(ctx, {
+new Chart(ctx, {
   type: 'bar',
   data: {
     labels: ['JAN', 'FEB', 'MARCH', 'APRIL', 'MAY', 'JUNE'],
     datasets: [
-    {
-      label: 'Borrowed Books',
-      data: [7, 10, 9, 12, 13, 6],
-      backgroundColor: 'rgba(117, 121, 231, .8)',
-    },
-    {
-      label: 'Returned Books',
-      data: [4, 7, 10, 11, 8, 8],
-      backgroundColor: 'rgba(154, 179, 245, .8)',
-    }
-  ],
+      {
+        label: 'Borrowed Books',
+        data: [7, 10, 9, 12, 13, 6],
+        backgroundColor: 'rgba(117, 121, 231, .8)',
+      },
+      {
+        label: 'Returned Books',
+        data: [4, 7, 10, 11, 8, 8],
+        backgroundColor: 'rgba(154, 179, 245, .8)',
+      },
+    ],
   },
   options: {
     scales: {
@@ -29,10 +29,11 @@ var myChart = new Chart(ctx, {
 });
 
 var pie = document.getElementById('pieChart').getContext('2d');
-var pieChart = new Chart(pie, {
+new Chart(pie, {
   type: 'doughnut',
   data: {
-    datasets: [{
+    datasets: [
+      {
         data: [10, 20, 30, 20],
         backgroundColor: [
           'rgb(52, 152, 219, .8)',
@@ -40,13 +41,14 @@ var pieChart = new Chart(pie, {
           'rgba(27, 206, 230, .8)',
           'rgba(27, 64, 230, .8)',
         ],
-    }],
+      }
+    ],
 
     labels: [
-        'Novel',
-        'Fiction',
-        'Biography',
-        'Horror',
+      'Novel',
+      'Fiction',
+      'Biography',
+      'Horror',
     ]
   },
   options: {
